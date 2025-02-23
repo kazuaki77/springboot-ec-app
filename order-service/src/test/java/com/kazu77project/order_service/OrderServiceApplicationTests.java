2a650f75680d0df1efec2a914620074d9aa786a4
Kazuaki Kawahara <Kazuaki.Kawahara@ibm.com>
2025-02-23 15:05:16 +0900

    add 3 projects

---
+++ order-service/src/test/java/com/kazu77project/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazu77project.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
